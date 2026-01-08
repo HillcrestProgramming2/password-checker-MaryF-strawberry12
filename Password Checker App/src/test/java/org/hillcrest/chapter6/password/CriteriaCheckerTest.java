b1c94e84933163a0ec2859b7d171e87f8c81cb48
MaryF-strawberry12 <flaming.emu29@gmail.com>
2026-01-08 08:10:29 -0700

    Changed one word

--- Password Checker App/src/test/java/org/hillcrest/chapter6/password/CriteriaCheckerTest.java
+++ Password Checker App/src/test/java/org/hillcrest/chapter6/password/CriteriaCheckerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Tests the CriteriaChecker Test
+ * Tests the CriteriaChecker class
  */
 public class CriteriaCheckerTest {
     @Test
